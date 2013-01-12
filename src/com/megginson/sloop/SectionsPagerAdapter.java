e9fde29e53ac888e13f9d43ac9f2627425b2c757
David Megginson <david.megginson@megginson.com>
2013-01-12 17:56:24 -0500

    Removed section fragment and pager adapter classes from main activity class, to simplify future development.
    Use 10 sections instead of 3, and skip some unneeded string resources.

---
+++ src/com/megginson/sloop/SectionsPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.megginson.sloop;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+	public SectionsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		// getItem is called to instantiate the fragment for the given page.
+		// Return a DummySectionFragment (defined as a static inner class
+		// below) with the page number as its lone argument.
+		Fragment fragment = new DummySectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// Show 3 total pages.
+		return 10;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		return "Section #" + (position + 1);
+	}
+}
+
