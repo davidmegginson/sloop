48cdbc1fa661fe93eda072af7afbab6e20f037ac
David Megginson <david.megginson@megginson.com>
2013-02-09 16:26:15 -0500

    Empty branch.

--- src/com/megginson/sloop/activities/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Activities (top-level pages) for Sloop.
- * 
- * The starting screen is {@link com.megginson.sloop.activities.MainActivity}.
- * 
- * @author David Megginson
- */
-package com.megginson.sloop.activities;
--- src/com/megginson/sloop/ui/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Sloop non-activity Android UI classes.
- * 
- * @author David Megginson
- */
-package com.megginson.sloop.ui;
