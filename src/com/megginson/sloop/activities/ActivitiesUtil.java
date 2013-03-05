f3e90de2522d8335f7f0474838880592f229944d
David Megginson <david.megginson@megginson.com>
2013-03-04 22:58:13 -0500

    Add missing files from last commit.

---
+++ src/com/megginson/sloop/activities/ActivitiesUtil.java
@@ -0,0 +1,59 @@
+package com.megginson.sloop.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Utility methods for activities.
+ * 
+ * @author David Megginson
+ */
+class ActivitiesUtil {
+
+	/**
+	 * URL for general help.
+	 */
+	protected final static String HELP_URL = "http://sloopdata.org";
+
+	/**
+	 * Show general help in the browser.
+	 * 
+	 * @param fromActivity
+	 *            the source activity.
+	 */
+	protected static void doHelp(Activity fromActivity) {
+		doLaunchBrowser(fromActivity, HELP_URL);
+	}
+
+	/**
+	 * End this activity and launch a non-CSV URL.
+	 * 
+	 * This method invokes {@link #finish()}.
+	 * 
+	 * @param fromActivity
+	 *            the source activity.
+	 * @param url
+	 *            The URL to launch in the browser (etc.).
+	 */
+	protected static void doLaunchBrowser(Activity fromActivity, String url) {
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		intent.setData(Uri.parse(url));
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		fromActivity.startActivity(intent);
+	}
+	
+	/**
+	 * Hide the soft keyboard.
+	 */
+	protected static void doHideKeyboard(Activity fromActivity, View view) {
+		InputMethodManager imm = (InputMethodManager) fromActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+	}
+
+
+
+}
