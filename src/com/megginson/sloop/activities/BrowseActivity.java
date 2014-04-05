ab44cc16f20737b4caa4b99816d30ebbc603bd92
David Megginson <david.megginson@megginson.com>
2014-04-05 16:37:20 -0400

    Basic tab switching working as a demo, but not saving fragment state (which is probably OK).

--- src/com/megginson/sloop/activities/BrowseActivity.java
+++ src/com/megginson/sloop/activities/BrowseActivity.java
@@ -1,17 +1,20 @@
 package com.megginson.sloop.activities;
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.app.ActionBar.Tab;
 
-public class BrowseActivity extends Activity
-{
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BrowseActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		// TODO: Implement this method
 		super.onCreate(savedInstanceState);
 		getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
@@ -19,42 +22,62 @@ public class BrowseActivity extends Activity
 		doOpen("Tab 2", "");
 		doOpen("Tab 3", "");
 	}
-	
+
 	@Override
-	public boolean onPrepareOptionsMenu(Menu menu)
-	{
+	public boolean onPrepareOptionsMenu(Menu menu) {
 		// TODO: Implement this method
 		return super.onPrepareOptionsMenu(menu);
 	}
-	
+
 	private void doOpen(String title, String url) {
 		ActionBar actionBar = getActionBar();
 		ActionBar.Tab tab = actionBar.newTab().setText(title);
-		tab.setTabListener(new TabListener());
+		tab.setTabListener(new TabListener(new DummyFragment()));
 		actionBar.addTab(tab, true);
 	}
-	
-	private class TabListener implements ActionBar.TabListener
-	{
+
+	public static class DummyFragment extends Fragment {
 
 		@Override
-		public void onTabSelected(ActionBar.Tab p1, FragmentTransaction p2)
-		{
-			// TODO: Implement this method
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			TextView textView = new TextView(container.getContext());
+			textView.setText("Hello, world! " + Math.random());
+			return textView;
+		}
+
+	}
+
+	private class TabListener implements ActionBar.TabListener {
+
+		private Fragment mFragment;
+		private boolean mFragmentAdded;
+
+		public TabListener(Fragment fragment) {
+			super();
+			mFragment = fragment;
+			mFragmentAdded = false;
 		}
 
 		@Override
-		public void onTabReselected(ActionBar.Tab p1, FragmentTransaction p2)
-		{
-			// TODO: Implement this method
+		public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+			if (!mFragmentAdded) {
+				ft.add(android.R.id.content, mFragment);
+				mFragmentAdded = true;
+			} else {
+				ft.attach(mFragment);
+			}
 		}
 
 		@Override
-		public void onTabUnselected(ActionBar.Tab p1, FragmentTransaction p2)
-		{
-			// TODO: Implement this method
+		public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 		}
-		
+
+		@Override
+		public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+			ft.detach(mFragment);
+		}
+
 	};
 
 }
